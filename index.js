24daf2c4852bd0e2575a95f2adebd0b568bd4812
Denys Korniienko <39014032+dkorni@users.noreply.github.com>
2024-04-15 00:46:01 +0300

    Created product controller

--- index.js
+++ index.js
@@ -1,65 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Product = require('./models/product.model')
+const productRoute = require('./routes/product.route')
 const app = express()
 
 // plugin middleware
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send("Hello from node api");
-})
-
-app.get('/api/products', async(req,res)=>{
-    try{
-        var products = await Product.find({});
-        res.status(200).json(products)
-    }catch(error){
-        console.error(error);
-        res.status(500).json({message: error.message});
-    }
-})
-
-app.get('/api/products/:id', async(req,res)=>{
-    try{
-        const { id } = req.params;
-        var products = await Product.findById(id);
-        res.status(200).json(products)
-    }catch(error){
-        console.error(error);
-        res.status(500).json({message: error.message});
-    }
-})
-
-app.post("/api/products", async (req, res)=>{
-
-    try{
-        const product = await Product.create(req.body);
-        res.status(200).json(product)
-    }catch(error){
-        console.error(error);
-        res.status(500).json({message: error.message});
-    }
-});
-
-app.put("/api/products/:id", async (req, res)=>{
-
-    try{
-        const { id } = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body);
-        
-        if(!product){
-            return res.status(404).json({message:"Product not found"})
-        }
-        
-        const updatedProduct = await Product.findById(id);
-
-        res.status(200).json(updatedProduct)
-    }catch(error){
-        console.error(error);
-        res.status(500).json({message: error.message});
-    }
-});
+// routes
+app.use("/api/products", productRoute);
 
 mongoose.connect('mongodb+srv://dencreativkor:meFVK7yHT7uYdHZ9@cluster0.z0k3fsx.mongodb.net/doc-to-pdf-api?retryWrites=true&w=majority&appName=Cluster0')
 .then(()=>{
